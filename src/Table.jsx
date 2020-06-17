04d87bf0e8dcfa284f06084188fde3d47092e174
p.wysocki <p.wysocki@infinity-group.pl>
2020-06-17 16:14:19 +0200

    key fixes

--- src/Table.jsx
+++ src/Table.jsx
@@ -13,13 +13,13 @@ class Table extends React.Component {
     return (
       <div>
         {this.props.rows.map((row, index) => (
-          <div className="Box-row d-flex flex-justify-between">
+          <div className="Box-row d-flex flex-justify-between" key={index}>
             {row}
             <input
               className="form-control input-sm"
               type="text"
               placeholder={this.getValue(this.props.data, name, row)}
-              onChange={event =>
+              onChange={(event) =>
                 this.props.addFunction(name, row, event.target.value)
               }
             />
@@ -32,7 +32,7 @@ class Table extends React.Component {
     return (
       <div>
         {this.props.tableNames.map((name, index) => (
-          <div className="pt-6">
+          <div className="pt-6" key={index}>
             <div className="Box col-6 mx-auto">
               <div className="Box-header Box-title">{name}</div>
               <div>{this.createRows(name)}</div>
